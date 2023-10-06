e13a8ff94ce65c5e3b65631dd9add554a0702e2d
WesleyOxman <143106468+WesleyOxman@users.noreply.github.com>
2023-10-06 08:18:51 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#Actually running the code
+
+from TexasHoldem import TexasHoldem
+
+
+def main():
+  TexasHoldem1 = TexasHoldem()
+  TexasHoldem1.getPlayerStart()
+  TexasHoldem1.getstartingBet()
+  TexasHoldem1.playerHands()
+  TexasHoldem1.startingRound()
+  TexasHoldem1.roundFlop()
+  TexasHoldem1.roundTurn()
+  TexasHoldem1.roundRiver()
+  TexasHoldem1.endGame()
+main()
